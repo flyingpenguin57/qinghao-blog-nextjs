2d0e9cc164569e7bbf026c11b555d7316d9fc3b7
hulio <597314452@qq.com>
2025-08-20 20:35:46 +0800

    bug fix

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -13,20 +13,3 @@ export const parseCustomMarkup = (input: string): Block[] => {
   return blocks;
 };
 
-// 下划线转驼峰
-function snakeToCamel(str: string): string {
-  return str.replace(/_([a-z])/g, (_, letter) => letter.toUpperCase());
-}
-
-// 通用转换函数
-function keysToCamel<T>(obj: any): T {
-  if (Array.isArray(obj)) {
-    return obj.map(v => keysToCamel(v)) as unknown as T;
-  } else if (obj !== null && obj.constructor === Object) {
-    return Object.fromEntries(
-      Object.entries(obj).map(([k, v]) => [snakeToCamel(k), keysToCamel(v)])
-    ) as T;
-  }
-  return obj;
-}
-
