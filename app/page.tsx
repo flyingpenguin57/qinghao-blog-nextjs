8c4e443dae3d554baf9d31648f3f05c46b133ce8
hulio <597314452@qq.com>
2025-08-20 21:47:44 +0800

    bug fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,12 @@
 import ArticleList from '@/app/ui/components/ArticleList';
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
+interface PageProps {
+  searchParams?: { [key: string]: string | string[] | undefined }
 }
-) {
+
+export default async function Page({ searchParams }: PageProps) {
   const params = await searchParams; // 如果 Next.js 提示需要 await
-  const page = params.page ? parseInt(params.page as string, 10) : 1;  
+  const page = params?.page ? parseInt(params.page as string, 10) : 1;  
   return (
     <ArticleList page={page} />
   );
