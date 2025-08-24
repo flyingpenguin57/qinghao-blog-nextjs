7db56fad753cf5c914346b175b45d58d26911c03
hulio <597314452@qq.com>
2025-08-24 11:34:20 +0800

    bug fix

--- app/(pages)/edit/page.tsx
+++ app/(pages)/edit/page.tsx
@@ -2,11 +2,11 @@ import { fetchArticleById } from '@/app/lib/data';
 import EditClientPage from '../../ui/components/EditClientPage';
 
 interface PageProps {
-  searchParams?: { id?: string };
+  params: Promise<{ id: string }>
 }
 
-export default async function Page({ searchParams }: PageProps) {
-  const id = searchParams?.id;
+export default async function Page({ params }: PageProps) {
+  const { id } = await params;
   let article = null;
 
   if (id) {
